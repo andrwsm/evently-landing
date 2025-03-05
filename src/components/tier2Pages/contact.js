cdb1931e0ab0a6c2e04f9204aac1d130da51f3e0
Andrew <andrw.smth@outlook.com>
2025-03-05 17:49:26 +0000

    commit

--- src/components/tier2Pages/contact.js
+++ src/components/tier2Pages/contact.js
@@ -159,21 +159,7 @@ export default function Contact() {
           </div>
           <Field className="flex gap-x-4 sm:col-span-2">
             <div className="flex h-6 items-center">
-              {/* <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className="group flex w-8 flex-none cursor-pointer rounded-full
-                 bg-gray-200 p-px ring-1 ring-gray-900/5 transition-colors duration-200 
-                 ease-in-out ring-inset focus-visible:outline-2 focus-visible:outline-offset-2
-                  focus-visible:outline-dark-sky data-checked:bg-indigo-600"
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className="size-4 transform rounded-full bg-white ring-1 shadow-xs
-                   ring-gray-900/5 transition duration-200 ease-in-out group-data-checked:translate-x-3.5"
-                />
-              </Switch> */}
+   
             </div>
             <p>
               By selecting submitting your details, you agree to our <a href="/privacypolicy" className="font-semibold text-dark-sky">
